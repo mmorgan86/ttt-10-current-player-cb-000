7d229f3ba7e8f1fc79800414284908b2fa84aeec
Michael Morgan <michael1986morgan@gmail.com>
2018-06-01 23:05:34 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,5 @@
 #urn_count defines whose turn it is
 def turn_count(board)
-  counter = 0 
-  if board.length = "x" || board.length = "O"
-    count += 1
-    return count
-  end
+  board.each do |turn|
+    turn
 end
